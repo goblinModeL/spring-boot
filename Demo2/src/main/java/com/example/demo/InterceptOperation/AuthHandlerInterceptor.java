a630938fb98e7939dff28aa3bd4ba60d5e7e6565
ly <1563694059@qq.com>
2024-04-09 17:11:53 +0800

    WebSocket

--- Demo2/src/main/java/com/example/demo/InterceptOperation/AuthHandlerInterceptor.java
+++ Demo2/src/main/java/com/example/demo/InterceptOperation/AuthHandlerInterceptor.java
@@ -31,7 +31,7 @@ public class AuthHandlerInterceptor  implements HandlerInterceptor {
             return true;
         }
         // 如果是登录接口，则放行
-        if ("/home/login".equals(requestPath) || requestPath.startsWith("/getActive")) {
+        if ("/home/login".equals(requestPath) || requestPath.startsWith("/getActive")|| requestPath.startsWith("/websocket")) {
             return true;
         }
         System.out.println("11111");
